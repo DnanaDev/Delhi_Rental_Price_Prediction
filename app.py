c1dc9e888636f93a168fdeedc74d17b9d751173d
Anand <anand.2700@yahoo.in>
2020-11-06 14:31:45 +0530

    Fixed Drop-downs, refactored UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from Models.predict import predict_rent, columns
 
 # Master Variable through which everything is done
@@ -14,16 +14,15 @@ locality_names_list = [x.split('__')[1] for x in columns[12:]]
 # Routes for different parts of the site
 
 @app.route('/')
-def hello():
+def homepage():
     # Passing Python Variables to HTML using flask jinja template
 
-    return render_template('index.html', input_features=features, ln_list=locality_names_list)
+    return render_template('index.html')
 
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET', 'POST'])
 def input_data():
     if request.method == 'POST':
-        print(request.form)
         # Form here is a dictionary that contains the variables, with name as the key.
         size = int(request.form['size'])
         property_type = str(request.form['property_type'])
@@ -31,7 +30,10 @@ def input_data():
         locality_name = str(request.form['locality_name'])
         # final Predictions, calling the model
         predicted_rent = predict_rent(size, property_type, suburb_name, locality_name)
-    return render_template('result.html', monthly_rent=int(predicted_rent))
+        if predicted_rent:
+            return jsonify(predicted_rent=int(predicted_rent))
+
+    return render_template('result.html')
 
 
 if __name__ == '__main__':
